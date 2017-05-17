01b3ac5b0d64823a735c33b0ab7d66725124f1fa
Rama Dimasatria <ramadimasatria@gmail.com>
2017-05-18 01:34:50 +0700

    Update width & height

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -3,23 +3,29 @@ import LazyLoad from 'react-lazyload';
 
 import styles from './styles.css';
 
-const Photo = ({ url, name, country, row, col, tileData }) => (
-  <div className={styles.container}>
-    <LazyLoad offset={2 * tileData.cellSize}>
-      <img
-        src={url}
-        className={styles.image}
-        alt={name}
-        width={col * tileData.cellSize}
-      />
-    </LazyLoad>
+const Photo = ({ url, name, country, row, col, tileData }) => {
+  const width = col * tileData.cellSize;
+  const height = row * tileData.cellSize;
 
-    <div className={styles.caption}>
-      { name }
-      <br />
-      { country }
+  return (
+    <div className={styles.container}>
+      <LazyLoad offset={2 * tileData.cellSize} height={height}>
+        <img
+          src={url}
+          className={styles.image}
+          alt={name}
+          width={width}
+          height={height}
+        />
+      </LazyLoad>
+
+      <div className={styles.caption}>
+        { name }
+        <br />
+        { country }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Photo;
