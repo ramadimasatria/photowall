474d1d376aaf1f7f3ad80c9de92dc2ab26b775ef
Rama Dimasatria <ramadimasatria@gmail.com>
2017-05-19 00:52:18 +0700

    Update app name

--- server.js
+++ server.js
@@ -163,5 +163,5 @@ const server = app.listen(port, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('Essential React listening at http://%s:%s', host, port);
+  console.log('Photowall listening at http://%s:%s', host, port);
 });
